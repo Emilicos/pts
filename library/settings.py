c59f7e57965caafb2886f2d48679e29ff7dea6bc
emilicos <notalvaroaustin@gmail.com>
2023-10-21 20:03:58 +0700

    feat: change allow host

--- library/settings.py
+++ library/settings.py
@@ -27,7 +27,7 @@ PRODUCTION = env.bool('PRODUCTION', False)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
